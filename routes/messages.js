00d2240508d452623d661bd8b7272c213547a177
tarynries <taryn.ries@gmail.com>
2023-04-21 10:50:55 +0200

    updating the routes files

--- routes/messages.js
+++ routes/messages.js
@@ -1,3 +1,11 @@
+
+const Router = require("express").Router;
+const router = new Router();
+
+const Message = require("../models/message");
+const { ensureLoggedIn } = require("../middleware/auth");
+const ExpressError = require("../expressError");
+
 /** GET /:id - get detail of message.
  *
  * => {message: {id,
@@ -12,12 +20,42 @@
  **/
 
 
+
+router.get("/:id", ensureLoggedIn, async function (req, res, next) {
+    try {
+        let username = req.user.username;
+        let msg = await Message(req.params.id);
+
+        if (msg.to_user.username !== username && msg.from_user.username !== username) {
+            throw new ExpressError("Cannot read this message", 401);
+        }
+
+        return res.json({ message: msg })
+    } catch (err) {
+        return next(err);
+    }
+});
+
 /** POST / - post message.
  *
  * {to_username, body} =>
  *   {message: {id, from_username, to_username, body, sent_at}}
  *
  **/
+router.post("/", ensureLoggedIn, async function (req, res, next) {
+    try {
+        const msg = await Message.create({
+            from_username: req.user.username,
+            to_username: req.body.to_username,
+            body: req.body.body
+        });
+
+
+        return res.json({ message: msg })
+    } catch (err) {
+        return next(err);
+    }
+});
 
 
 /** POST/:id/read - mark message as read:
@@ -27,4 +65,25 @@
  * Make sure that the only the intended recipient can mark as read.
  *
  **/
+
+router.post("/:id/read", ensureLoggedIn, async function (req, res, next) {
+    try {
+        let username = req.user.username;
+        let msg = await Message(req.params.id);
+
+        if (msg.to_user.username != username) {
+            throw new ExpressError("Cannot set this message to read", 401);
+        }
+        let message = await Message.markRead(req.params.id);
+
+        return res.json({ message });
+
+
+    } catch (err) {
+        return next(err);
+    }
+});
+
+
+module.exports = router;
 
